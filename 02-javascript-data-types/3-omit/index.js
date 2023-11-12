0be4615cd93b0be8a2ec1687d32d1ea7f945f0f8
Богданов Сергей <s.bogdanov@platformaofd.ru>
2023-11-12 22:11:11 +0300

    02-03

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -4,17 +4,10 @@
  * @param {...string} fields - the properties paths to omit
  * @returns {object} - returns the new object
  */
-export const omit = (obj, ...fields) => {
-    let outObj = {};
 
-    // Попробуйте использовать spread оператор, вместо Object.assign
-    // https://github.com/js-tasks-ru/js-20231106_sbugs/pull/2
-    t o d o
-    Object.assign(outObj, obj);
+export const omit = (obj, ...fields) => {
     for(let itm in fields) {
-        if(obj[fields[itm]]) {
-            delete outObj[fields[itm]]
-        }
+        obj[fields[itm]] && delete obj[fields[itm]];
     }
-    return outObj;
+    return obj;
 };
