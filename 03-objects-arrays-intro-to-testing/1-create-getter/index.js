e8c46ec75d4a0242a675aff3a23b7606fa4516d5
Богданов Сергей <s.bogdanov@platformaofd.ru>
2023-11-17 23:55:04 +0300

    1-create-getter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,8 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+    const a = path.split(".");
+    return function (o) {
+        return a.reduce((o, k) => (o && k ? o[k] : undefined), o);
+    };
 }
